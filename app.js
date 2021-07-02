0e6e8aeb6db3783943ba1994c00df11d0ee5b008
k-ballard <86800230+k-ballard@users.noreply.github.com>
2021-07-01 20:08:42 -0700

    Added predicatePhrases and affirmations arrays. Added tests to make sure the arrays are correct.

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
 const nouns = [
-    "Life",
-    "Sensation",
+    'Life',
+    'Sensation',
     'Hope',
     'Love',
     'Peace',
     'Tranquility',
     'Resilience',
-    'Triumph'
+    'Triumph',
+    'Forgiveness'
 ];
 
+const predicatePhrases = [
+    'is beautiful.',
+    'is expansive.',
+    'is timeless.',
+    'is growth.',
+    'is longlasting.',
+    'is flexible.',
+    'is timeless.'
+];
+
+const affirmations = [
+    'Embrace it!',
+    'Cherish it!',
+    'Let it guide you!',
+    'Use it!',
+    'Allow yourself to be empowered by it!',
+    'Let it guide you!',
+    'Seek it out!'
+];
+
+// Tests that the arrays is correct.
 for(let i = 0; i < nouns.length; i++){
     console.log(nouns[i]);
-  };
+    };
+
+for(let i = 0; i < predicatePhrases.length; i++){
+    console.log(predicatePhrases[i]);
+    };
+
+for(let i = 0; i < affirmations.length; i++){
+    console.log(affirmations[i]);
+    };
