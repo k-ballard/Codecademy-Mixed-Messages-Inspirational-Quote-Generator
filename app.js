da6a9d618639b6751865ed246548e1e7a0fe8d5f
k-ballard <86800230+k-ballard@users.noreply.github.com>
2021-07-01 19:54:32 -0700

    Added predicatePhrases and affirmations arrays.

---
+++ app.js
@@ -0,0 +1,14 @@
+const nouns = [
+    "Life",
+    "Sensation",
+    'Hope',
+    'Love',
+    'Peace',
+    'Tranquility',
+    'Resilience',
+    'Triumph'
+];
+
+for(let i = 0; i < nouns.length; i++){
+    console.log(nouns[i]);
+  };
